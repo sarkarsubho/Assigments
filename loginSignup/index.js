88733c8214414552b7141b238a6a8e213143f773
sarkarsubho <93681750+sarkarsubho@users.noreply.github.com>
2022-08-28 12:45:21 +0530

    commit

--- loginSignup/index.js
+++ loginSignup/index.js
@@ -13,8 +13,12 @@ const connect = () => {
 const userSchema =new mongoose.Schema({
     firstname:{type:String,required:true},
     lastname:{type:String,required:true},
-    
+    email:{type:String,required:true,unique:true},
+    password:{type:String,required:true},
 })
+
+const User=mongoose.model("user",userSchema);
+
 
 app.listen(8080, async()=>{
     try
